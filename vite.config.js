a584b36db86017dd9a0c68b6de047559aa5e7b4d
FEYAZ <FEYAZKHAN8800@GMAIL.COM>
2025-11-22 09:47:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 🚨 ADD THIS LINE to fix asset loading on GitHub Pages
+  base: '/My-Portfolio/', 
   plugins: [react()],
 })
